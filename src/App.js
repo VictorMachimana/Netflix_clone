e1dc0f837277cdf29853ff811373d7eb6f309367
victor machimana <victormacrich@gmail.com>
2020-07-10 03:32:21 +0200

    add tmdb key and api, create request api routes, create row component, create row css, import row component to app.js, pass url's and title as props from app.js, create host on firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Row from "./Row";
+import requests from "./requests";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                Hello Clever Programmer! Let's build Netflix Clone Front end
+                today
+            </h1>
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={requests.fetchNetflixOriginals}
+            />
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Row.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        // e.g https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213
+
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+        // if [], run once then row loads, and don't run it again || or [movies] - it will run everytime movies changes(dependencies)
+    }, [fetchUrl]); //when ever your using any variable in the useEffect from outside useEffect you have to put it in the array, because it's a dependent
+
+    console.log(movies);
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row_posters">
+                {/* several row_poster */}
+
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
