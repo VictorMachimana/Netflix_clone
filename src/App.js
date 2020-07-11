30038b7a03aadebd75d9aa2c71224946b55457f9
victor machimana <victormacrich@gmail.com>
2020-07-11 07:10:46 +0200

    create banner component, create banner css, style banner, import banner to app js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
+import Banner from "./Banner";
 import Row from "./Row";
 import requests from "./requests";
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <h1>
-                Hello Clever Programmer! Let's build Netflix Clone Front end
-                today
-            </h1>
+        <div className="app">
+            {/* Nav */}
+            <Banner />
             <Row
                 title="NETFLIX ORIGINALS"
                 fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
             />
             <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+            <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
         </div>
     );
 }
